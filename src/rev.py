eaf6edacfbd6af9774e8a3605776355ccb2a14f5
Vraj Parikh <vrajparikh3001@gmail.com>
2024-04-18 11:12:05 +0530

    seperated main page

--- src/rev.py
+++ src/rev.py
@@ -7,21 +7,9 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Game Title")
 
-play_hover_sound = pygame.mixer.Sound("../Assets/audio/main_page/play_hover_sound.mp3")
-
-background = pygame.image.load("../Assets/sprites/main_page/main.jpg")
-background = pygame.transform.scale(background, (800, 600))
-
-font = pygame.font.Font("../Assets/sprites/main_page/play_font.ttf", 45)
 
 screen_name = "home page"
 
-text_size = 55
-play_text = "PLAY"
-play_button = font.render(play_text, True, (0, 0, 0))
-play_button_rect = play_button.get_rect(center=(400, 400))
-
-pause = 0
 
 clock = pygame.time.Clock()
 
@@ -33,25 +21,7 @@ while running:
             pygame.quit()
             sys.exit()
     if screen_name=="home page":
-        if event.type == pygame.MOUSEMOTION:
-            if play_button_rect.collidepoint(event.pos):
-                text_size = 70 
-                if(pause==0):
-                    play_hover_sound.play()
-                    pause+=1
-            else:
-                text_size = 55
-                pause=0
-        if event.type == pygame.MOUSEBUTTONDOWN and screen_name=="home page":
-            if play_button_rect.collidepoint(event.pos):
-                screen_name = "page 1"
-
-        screen.blit(background, (0, 0))
-
-        font = pygame.font.Font("../Assets/sprites/main_page/play_font.ttf", text_size)
-        play_button = font.render(play_text, True, (0, 0, 0))
-        play_button_rect = play_button.get_rect(center=(400, 400))
-        screen.blit(play_button,play_button_rect)
+        exec(open("home_page_locked.py").read())
 
     elif screen_name=="page 1":
         exec(open("factory.py").read())
